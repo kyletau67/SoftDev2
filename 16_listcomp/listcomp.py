22be3ef22df6866bd3d35d67b55833eaea6ebc58
Kyle Tau <kyletau2@gmail.com>
2019-04-11 21:44:23 -0400

    incomplete

---
+++ 16_listcomp/listcomp.py
@@ -0,0 +1,17 @@
+'''Kyle Tau
+SoftDev2 pd 6
+K16 -- Do You Even List?
+2019-04-11'''
+
+
+UC="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LC="abcdefghijklmnopqrstuvwxyz"
+NUMS="1234567890"
+pass="myNoobPass1234"
+
+def validator(p):
+	f = [1 for x in pass if x in UC and 2 for x in pass if x in LC and 3 for x in pass if x in NUMS]
+	return 
+
+def strength(p):
+	
